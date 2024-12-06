6046e5a1093e5963a2bfb781fa06a67eddddc869
Sanchay1054 <sanchaym.22cse@kongu.edu>
2024-12-06 14:21:18 +0530

    without routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Routing from './Routing/Routing';
+import Home from './Screen/Home Page/Home';
 
 function App() {
   return (
-    <Routing/>
+    <Home/>
   );
 }
 
